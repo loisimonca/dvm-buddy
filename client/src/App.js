0215e283a58e475617ac5f3c7457edd537c72e8c
songhee <thdgml5537@gmail.com>
2021-02-02 19:13:06 -0500

    build up the express server and connect with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 import Navbar from "./component/Navbar/Navbar";
-
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Navbar />
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-      </>
-    );
-  }
+function App() {
+  //express server api route test
+  useEffect(() => {
+    axios.get("/api/test").then((response) => {
+      console.log(response.data);
+    }, []);
+  });
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
