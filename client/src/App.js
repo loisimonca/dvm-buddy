8bd28c844251ddf56cba6b97b8eaf0b9cc19ecd4
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-02-04 15:10:35 -0500

    added BroweserRouter to dispaly navbar on everypage

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import API from "./utils/API";
 import Navbar from "./component/Navbar/Navbar";
 import Login from "./component/Login/Login";
+import CustomerAccount from "./component/CustomerAccount/CustomerAccount"
+import CustomerPage from "./component/CustomerPage/CustomerPage"
 function App() {
   //express server api route test (user info)
   //data from mongoDB will be displayed on the console
@@ -18,5 +20,16 @@ function App() {
     </>
   );
 }
+
+<BrowserRouter>
+    <div>
+        <Navbar/>
+        <Switch>
+            <Route exact path="/" component={CustomerAccount}/>
+            <Route path="/CustomerPage" component={CustomerPage} />                         
+            <Route path="*" render={() => <Redirect to="/" />} />
+        </Switch>
+    </div>
+</BrowserRouter>
 
 export default App;
