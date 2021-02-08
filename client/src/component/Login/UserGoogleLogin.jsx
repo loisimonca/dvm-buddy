f059dbdbb3055cd61ad279eae78c28c557755eac
songhee <thdgml5537@gmail.com>
2021-02-08 18:27:53 -0500

    add google / facebook login

---
+++ client/src/component/Login/UserGoogleLogin.jsx
@@ -0,0 +1,34 @@
+import React, {useContext} from 'react'
+import {GoogleLogin} from 'react-google-login';
+import {UserContext } from '../../utils/UserContext';
+
+//Refresh token
+import {refreshTokenSetup} from '../../utils/refreshToken';
+
+const clientId = "32479969633-uog46n0h5g22l1rps60k0cvbluuq3ni6.apps.googleusercontent.com"
+
+function UserGoogleLogin() {
+    const {value, setValue} = useContext(UserContext);
+
+    //google login  
+    const onSuccess = (res) =>{
+        console.log("[Login Success] currentUser: ", res.profileObj );
+        setValue("true")
+        //initializing the setup
+        refreshTokenSetup(res);
+    };
+    const onFailure = (res) =>{
+        console.log("[Login failed] res: ", res);
+    }
+
+    return (
+        <div>
+           <GoogleLogin clientId={clientId} 
+           render={renderProps =>(<button className="google-login" 
+           onClick={renderProps.onClick} disabled={renderProps.disabled}><i className="fab fa-google" aria-hidden="true"></i>Sign in with Google</button>)} 
+           buttonText='Login' onSuccess={onSuccess} onFailure={onFailure} cookiePolicy={'single_host_origin'} style={{ marginTop: '100px'}} />
+        </div>
+    )
+}
+
+export default UserGoogleLogin
