c8e41e99086fbdc6499b9fc547cd6d16dd60fea5
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-02-09 21:18:38 -0500

    added cards to customerpage and styled cards.

--- client/src/component/CustomerPage/CustomerPage.jsx
+++ client/src/component/CustomerPage/CustomerPage.jsx
@@ -1,6 +1,133 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./CustomerPage.css";
 
-// return (
-//     <>
+
+const CustomerPage = () => {
+
+return (
+<>
+<br>
+<h1>Welcome to your personal account! What would you like to do today?</h1>
+</br>
+<div className ="card">
+<div className ="card-image">
+  <figure className ="image is-4by3">
+    <img
+      src="https://bulma.io/images/placeholders/1280x960.png"
+      alt="Placeholder image"
+    />
+  </figure>
+</div>
+<div className ="card-content">
+  <div className ="media">
+    <div className ="media-left">
+      <figure className ="image is-48x48">
+        <img
+          src="https://bulma.io/images/placeholders/96x96.png"
+          alt="Placeholder image"
+        />
+      </figure>
+    </div>
+    <div className ="media-content">
+      <p className ="title is-4"></p>
+      <p className ="subtitle is-6"></p>
+    </div>
+  </div>
+
+  <div className ="content">
+    
+  </div>
+</div>
+</div>
+<div className ="card">
+<div className ="card-image">
+  <figure className ="image is-4by3">
+    <img
+      src="https://bulma.io/images/placeholders/1280x960.png"
+      alt="Placeholder image"
+    />
+  </figure>
+</div>
+<div className ="card-content">
+  <div className ="media">
+    <div className ="media-left">
+      <figure className ="image is-48x48">
+        <img
+          src="https://bulma.io/images/placeholders/96x96.png"
+          alt="Placeholder image"
+        />
+      </figure>
+    </div>
+    <div className ="media-content">
+      <p className ="title is-4"></p>
+      <p className ="subtitle is-6"></p>
+    </div>
+  </div>
+
+  <div className ="content">
+  </div>
+</div>
+</div>
+<div className ="card">
+<div className ="card-image">
+  <figure className ="image is-4by3">
+    <img
+      src="https://bulma.io/images/placeholders/1280x960.png"
+      alt="Placeholder image"
+    />
+  </figure>
+</div>
+<div className ="card-content">
+  <div className ="media">
+    <div className ="media-left">
+      <figure className ="image is-48x48">
+        <img
+          src="https://bulma.io/images/placeholders/96x96.png"
+          alt="Placeholder image"
+        />
+      </figure>
+    </div>
+    <div className ="media-content">
+      <p className ="title is-4"></p>
+      <p className ="subtitle is-6"></p>
+    </div>
+  </div>
+
+  <div className ="content">
+  </div>
+</div>
+</div>
+<div className ="card">
+<div className ="card-image">
+  <figure className ="image is-4by3">
+    <img
+      src="https://bulma.io/images/placeholders/1280x960.png"
+      alt="Placeholder image"
+    />
+  </figure>
+</div>
+<div className ="card-content">
+  <div className ="media">
+    <div className ="media-left">
+      <figure className ="image is-48x48">
+        <img
+          src="https://bulma.io/images/placeholders/96x96.png"
+          alt="Placeholder image"
+        />
+      </figure>
+    </div>
+    <div className ="media-content">
+      <p className ="title is-4"></p>
+      <p className ="subtitle is-6"></p>
+    </div>
+  </div>
+
+  <div className ="content">
+  </div>
+</div>
+</div>
+</>
+);
+};
+
+export default CustomerPage;
