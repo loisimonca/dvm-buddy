8bc34a93eb0f542e74061b223df5074d8969621b
chyppoli <claude.hyppolite@gmail.com>
2021-02-10 21:18:58 -0500

    formated customerpage.jsx

--- client/src/component/CustomerPage/CustomerPage.jsx
+++ client/src/component/CustomerPage/CustomerPage.jsx
@@ -1,107 +1,120 @@
-import React from "react";;
+import React from "react";
 import "./CustomerPage.css";
 
-
-    const CustomerPage = (props) => {
-   
-        return (
-<div>
-<div className ="card">
-  <div className ="card-image">
-    <figure className ="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-    </figure>
-  </div>
-  <div className ="card-content">
-    <div className ="media">
-      <div className ="media-left"></div>
-      <div className ="media-content">
-        <p className ="title is-4"></p>
-        <p className ="subtitle is-6"></p>
+const CustomerPage = (props) => {
+  return (
+    <div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src="https://bulma.io/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left"></div>
+            <div className="media-content">
+              <p className="title is-4"></p>
+              <p className="subtitle is-6"></p>
+            </div>
+          </div>
+          <div className="content">
+            <br />
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src="https://bulma.io/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left"></div>
+            <div className="media-content">
+              <p className="title is-4"></p>
+              <p className="subtitle is-6"></p>
+            </div>
+          </div>
+          <div className="content">
+            <br />
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src="https://bulma.io/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left"></div>
+            <div className="media-content">
+              <p className="title is-4"></p>
+              <p className="subtitle is-6"></p>
+            </div>
+          </div>
+          <div className="content">
+            <br />
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src="https://bulma.io/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left"></div>
+            <div className="media-content">
+              <p className="title is-4"></p>
+              <p className="subtitle is-6"></p>
+            </div>
+          </div>
+          <div className="content">
+            <br />
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src="https://bulma.io/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left"></div>
+            <div className="media-content">
+              <p className="title is-4"></p>
+              <p className="subtitle is-6"></p>
+            </div>
+          </div>
+          <div className="content">
+            <br />
+          </div>
+        </div>
       </div>
     </div>
-    <div className ="content">
-      <br/>
-    </div>
-  </div>
-</div>
-<div className ="card">
-  <div className ="card-image">
-    <figure className ="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-    </figure>
-  </div>
-  <div className ="card-content">
-    <div className ="media">
-      <div className ="media-left"></div>
-      <div className ="media-content">
-        <p className ="title is-4"></p>
-        <p className ="subtitle is-6"></p>
-      </div>
-    </div>
-    <div className ="content">
-      <br/>
-    </div>
-  </div>
-</div>
-<div className ="card">
-  <div className ="card-image">
-    <figure className ="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-    </figure>
-  </div>
-  <div className ="card-content">
-    <div className ="media">
-      <div className ="media-left"></div>
-      <div className ="media-content">
-        <p className ="title is-4"></p>
-        <p className ="subtitle is-6"></p>
-      </div>
-    </div>
-    <div className ="content">
-      <br/>
-    </div>
-  </div>
-</div>
-<div className ="card">
-  <div className ="card-image">
-    <figure className ="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-    </figure>
-  </div>
-  <div className ="card-content">
-    <div className ="media">
-      <div className ="media-left"></div>
-      <div className ="media-content">
-        <p className ="title is-4"></p>
-        <p className ="subtitle is-6"></p>
-      </div>
-    </div>
-    <div className ="content">
-      <br/>
-    </div>
-  </div>
-</div>
-<div className ="card">
-  <div className ="card-image">
-    <figure className ="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-    </figure>
-  </div>
-  <div className ="card-content">
-    <div className ="media">
-      <div className ="media-left"></div>
-      <div className ="media-content">
-        <p className ="title is-4"></p>
-        <p className ="subtitle is-6"></p>
-      </div>
-    </div>
-    <div className ="content">
-      <br/>
-    </div>
-  </div>
-</div>
-</div>
-);
+  );
 };
 export default CustomerPage;
