460abcc784d4d3486564ec3f19dcda6aa4a933aa
chyppoli <claude.hyppolite@gmail.com>
2021-02-10 21:44:48 -0500

    add client routes for appointments

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,4 +16,24 @@ export default {
   getZipCode: function (currentZip, distance) {
     return axios.get(`/api/classified/zipcode/${currentZip}/${distance}`);
   },
+  //grabs all available appointments to display
+  getAvailAppts: function () {
+    return axios.get("/api/appointments/available");
+  },
+  //needs _id from user collection
+  getApptByCustomer: function (userId) {
+    return axios.get(`/api/appointments/customer/${userId}`);
+  },
+  //needs id of schedule
+  deleteAppt: function (apptId) {
+    return axios.delete(`/api/appointments/${apptId}`);
+  },
+  //userId is the _id from user table
+  setAppt: function (apptId, userId) {
+    return axios.put(`/api/appointments/${apptId}`, userId);
+  },
+  //route needs apptDate formatted as "YYYY-MM-DD" and apptTime formatted as "hh:mm"
+  createAppt: function (data) {
+    return axios.post("/api/appointments/", data);
+  },
 };
--- controllers/appointmentsController.js
+++ controllers/appointmentsController.js
@@ -33,6 +33,7 @@ module.exports = {
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+  //works
   setAppointment: (req, res) => {
     console.log(req.body.user);
     db.Appointment.findByIdAndUpdate(req.params.id, {
