4118cd9b9a8104ce12402675245d601870c7f615
chyppoli <claude.hyppolite@gmail.com>
2021-02-13 09:45:35 -0500

    remove space in admin directory name

--- client/src/component/AppointmentPage/AppointmentPage.js
+++ client/src/component/AppointmentPage/AppointmentPage.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
 import moment from "moment";
-import axios from "axios";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 const AppointmentPage = () => {
   const [appointments, setAppointments] = useState([]);
@@ -9,68 +11,101 @@ const AppointmentPage = () => {
   const defaultTime = moment().format("HH:mm");
   const userId = localStorage.getItem("userId");
 
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [confirmedAppointment, setconfirmedAppointment] = useState({});
+
   useEffect(() => {
     API.getAvailAppts()
       .then((response) => {
-
-        const filteredData = response.data.filter((appointment) => appointment.apptDate >= defaultDate && appointment.apptTime > defaultTime);
+        const filteredData = response.data.filter(
+          (appointment) =>
+            appointment.apptDate >= defaultDate &&
+            appointment.apptTime > defaultTime
+        );
         setAppointments(filteredData);
-        
-
       })
       .catch((err) => console.log(err));
   }, []);
 
-
+  //filters available appointments list based on date input
   const filterAppointments = (date) => {
-    console.log("date input is ", date)
-   const filteredData = appointments.filter((appointment) => appointment.apptDate >= date && appointment.apptTime > defaultTime);
-        setAppointments(filteredData);
-  }
+    console.log("date input is ", date);
+    const filteredData = appointments.filter(
+      (appointment) =>
+        appointment.apptDate >= date && appointment.apptTime > defaultTime
+    );
+    setAppointments(filteredData);
+  };
 
+  function handleAppointmentConfirmation(apptDate, apptTime, scheduleId) {
+    const formattedDisplayDate = moment(apptDate, "YYYY-MM-DD").format(
+      "dddd, MMMM do YYYY"
+    );
+    const formattedDisplayTime = moment(apptTime, "HH:mm").format("h:mm a");
+
+    setconfirmedAppointment({ formattedDisplayDate, formattedDisplayTime });
+
+    console.log(confirmedAppointment);
+
+    setModalIsOpen(true);
+  }
   //table that holds all available appointments
   function createTable() {
-  
+    const table = [];
 
-  const table = [];
+    console.log("calling availAppoints from createTable ", appointments);
+    console.log("userid is ", userId);
 
-  console.log("calling availAppoints from createTable ", appointments);
-  console.log('userid is ', userId);
+    const newAppointmentArray = appointments.reduce((r, a) => {
+      r[a.apptDate] = r[a.apptDate] || [];
+      r[a.apptDate].push({ time: a.apptTime, id: a._id });
+      return r;
+    }, {});
 
-  const newAppointmentArray = appointments.reduce((r, a) => {
-    r[a.apptDate] = r[a.apptDate] || [];
-    r[a.apptDate].push({ time: a.apptTime, id: a._id });
-    return r;
-  }, {});
+    console.log("New Appointment Array is ", newAppointmentArray);
 
-  console.log("New Appointment Array is ", newAppointmentArray);
+    for (let slots in newAppointmentArray) {
+      let children = [];
+      let dateHeader = slots;
 
-  for (let slots in newAppointmentArray) {
-    let children = [];
-    let dateHeader = slots;
+      newAppointmentArray[slots].map((item) => {
+        children.push(
+          <li
+            className="button is-small m-1"
+            data-id={item.id}
+            key={item.id}
+            data-display-date={slots}
+            data-display-time={item.time}
+            onClick={(e) =>
+              handleAppointmentConfirmation(
+                e.target.getAttribute("data-display-date"),
+                e.target.getAttribute("data-display-time"),
+                e.target.getAttribute("data-id")
+              )
+            }
+          >
+            {" "}
+            {item.time}{" "}
+          </li>
+        );
+        return children;
+      });
 
-    newAppointmentArray[slots].map((item) => {
-
-      children.push(
-        <li className="button is-small m-1"  data-id={item.id} key={item.id} 
-        
-        >
-          {" "}
-          {item.time}{" "}
-        </li>
+      table.push(
+        <ul className="box">
+          <h1 className="title">
+            {" "}
+            {moment(dateHeader, "YYYY-MM-DD").format("dddd, MMMM Do YYYY")}{" "}
+          </h1>{" "}
+          {children}{" "}
+        </ul>
       );
-      return children;
-    });
+    }
 
-    table.push(
-      <ul className="box">
-        <h1 className="title"> {moment(dateHeader,"YYYY-MM-DD").format("dddd, MMMM Do YYYY")} </h1> {children}{" "}
-      </ul>
-    );
-  }
+    function confirmAppt(params) {}
 
-  // console.log(table);
-  return table;
+    // console.log(table);
+    return table;
   }
 
   return (
@@ -79,18 +114,46 @@ const AppointmentPage = () => {
         <h1 className="title"> Available Schedules</h1>
         <div className="container">
           <div className="column is-half">
-            <label for="appointment-filter" className="label">Search from:</label>
+            <label htmlFor="appointment-filter" className="label">
+              Search from:
+            </label>
             <hr></hr>
-            <input 
-            type="date" 
-            defaultValue={defaultDate} 
-            onChange={(e) => filterAppointments(e.target.value)}
-            name="appointment-filter"
+            <input
+              type="date"
+              defaultValue={defaultDate}
+              onChange={(e) => filterAppointments(e.target.value)}
+              name="appointment-filter"
             />
           </div>
           <div className="column is-four-fifths">{createTable()}</div>
         </div>
       </section>
+      <div className="container">
+        <div className="column is-one-third">
+          <Modal isOpen={modalIsOpen}
+          style={{
+            overlay: {
+              backgroundColor: 'grey'
+            }
+          }}>
+            <h1 className="title">Appointment Confirmation</h1>
+            <p>
+              Please verify this is the date and time you would like to schedule
+              your appointment for. If correct click the Schedule button or
+              click the cancel buttom to start over
+            </p>
+            <br />
+            <br />
+            <p>Appointment Date: {confirmedAppointment.formattedDisplayDate}</p>
+            <p>Appointment Time: {confirmedAppointment.formattedDisplayTime}</p>
+
+            <button type="button">Schedule</button>
+            <button type="button" onClick={() => setModalIsOpen(false)}>
+              Cancel
+            </button>
+          </Modal>
+        </div>
+      </div>
     </div>
   );
 };
