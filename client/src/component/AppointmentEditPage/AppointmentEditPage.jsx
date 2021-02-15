17dea45d095b9c79d5375a413f498ea411e3f6fd
chyppoli <claude.hyppolite@gmail.com>
2021-02-15 17:02:49 -0500

    add edit cancel and save buttons

--- client/src/component/AppointmentEditPage/AppointmentEditPage.jsx
+++ client/src/component/AppointmentEditPage/AppointmentEditPage.jsx
@@ -16,27 +16,71 @@ const AppointmentEditPage = () => {
     },
   ]);
 
+//api call to retrieve list of appointments from server
+
+  function getAppointments () {
+    API.listAllAppointments()
+    .then((response) => {
+      const filteredData = response.data.filter(
+        (appointment) => appointment.apptDate >= defaultDate
+      )
+      setAppointments(filteredData);
+      setAppointmentList(filteredData);
+    });
+  }
+
+
+
+  const [inEditMode, setinEditMode] = useState({
+    status: false,
+    rowKey: null,
+  });
+  //hold customer id of row being edited
+  const [customerId, setcustomerId] = useState(null);
+
+  const onEdit = ({ _id, currentCustomerId }) => {
+    setinEditMode({
+      status: true,
+      rowKey: _id,
+    });
+    setcustomerId(currentCustomerId);
+  };
+
+  const updateAppointment = ({_id,newCustomerId}) => {
+      API.setAppt(_id,newCustomerId)
+      .then((resp) => console.log(resp))
+      .catch(err=> console.error(err))
+  }
+
   const defaultDate = moment().format("YYYY-MM-DD");
 
+  //get list of appointments from server
   const getApptlist = () => {
     API.listAllAppointments()
       .then((response) => {
         const data = response.data;
         setAppointmentList(data);
+        console.log("resp ", response.data)
       })
       .catch((err) => console.error(err));
   };
 
-  useEffect(() => {
-    API.listAllAppointments()
-      .then((response) => {
-        const filteredData = response.data.filter(
-          (appointment) => appointment.apptDate >= defaultDate
-        );
-        setAppointments(filteredData);
-        setAppointmentList(filteredData);
+  const onSave = ({_id,newCustomerId}) => {
+    updateAppointment({_id,newCustomerId});
+  }
+
+  const onCancel = () => {
+      setinEditMode({
+          status: false,
+          rowKey: null
       })
-      .catch((err) => console.log(err));
+      setcustomerId(null);
+  }
+
+
+
+  useEffect(() => {
+    getAppointments();
   }, []);
 
   function createTableBody() {
@@ -45,39 +89,65 @@ const AppointmentEditPage = () => {
 
     appointments.map((item) => {
       children.push(
-        <tr key={item._id}>
+        <tr {...item} key={item._id}>
           <td>{item.apptDate}</td>
           <td>{item.apptTime}</td>
           <td>{item.user ? item.user.email : ""}</td>
+          <td>
+                                {
+                                    inEditMode.status && inEditMode.rowKey === item.id ? (
+                                        <>
+                                            <button
+                                                className={"button is-success"}
+                                                onClick={() => onSave({id: item._id, newCustomerId: customerId})}
+                                            >
+                                                Save
+                                            </button>
+
+                                            <button
+                                                className={"button is-secondary"}
+                                                style={{marginLeft: 8}}
+                                                onClick={() => onCancel()}
+                                            >
+                                                Cancel
+                                            </button>
+                                        </>
+                                    ) : (
+                                        <button
+                                            className={"button is-primary"}
+                                            onClick={() => onEdit({id: item._id, currentCustomerId: item.customerId})}
+                                        >
+                                            Edit
+                                        </button>
+                                    )
+                                }
+                            </td>
         </tr>
       );
       return children;
     });
 
     table.push(
-        <>
+      <>
         <thead>
           <tr>
             <th>Date</th>
             <th>Time</th>
             <th>Customer Email</th>
+            
           </tr>
         </thead>
         <tbody>{children}</tbody>
-        </>
+      </>
     );
     return table;
   }
 
   return (
     <div className="container">
-        <div className="section">
-        <table className="table">
-      {createTableBody()}
-      </table>
-        </div>
-     
-
+      <div className="section">
+        <table className="table">{createTableBody()}</table>
+      </div>
     </div>
   );
 };
