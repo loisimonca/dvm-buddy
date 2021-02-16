6fc931ac0c16a5f7b7083c40528bec34170ab763
songhee <thdgml5537@gmail.com>
2021-02-16 14:28:51 -0500

    home landing page svg icon added

--- client/src/component/UserHomePage/UserHomePage.jsx
+++ client/src/component/UserHomePage/UserHomePage.jsx
@@ -13,13 +13,33 @@ const UserHomePage = () => {
         <video src="/videos/dog.mp4" autoPlay loop muted />
       </div>
       <div className="user__landing__page container">
-        <h2 className="user__landing__page__title">Welcome {name}!</h2>
-        <div className="user__landing__page__body">
-          <div className="user__landing__page__subtitle">
-           Visit your Account to see more options
-           </div>
-           <NavLink to='/AccountManage' className="button is-large">Account</NavLink>
+        <h2 className="user__landing__page__title">Welcome {name}</h2>
+
+        <div className="user__landing__page__description__container">
+          <button className="user__landing__page__button">
+            <img src="/images/veterinarian.svg" alt="veterianarian" />
+            Veterinary Care
+          </button>
+          <button className="user__landing__page__button">
+            <img src="/images/pawprints.svg" alt="pawprints" />
+            Dog Walking
+          </button>
+          <button className="user__landing__page__button">
+            <img src="/images/baby-sitter.svg" alt="baby-sitter" />
+            Pet Sitter
+          </button>
+          <button className="user__landing__page__button">
+            <img src="/images/beach.svg" alt="beach" />
+            Pet Boarding
+          </button>
         </div>
+
+          <div className="user__landing__page__body">
+            {/* <div className="user__landing__page__subtitle">
+            Visit your Account to see more options
+            </div> */}
+            <NavLink to='/AccountManage' className="user__landing__page__account__button is-large">Account</NavLink>
+          </div>
       </div>
     </div>
   );
