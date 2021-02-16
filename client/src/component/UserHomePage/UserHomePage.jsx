cac69aa36b3cd3d83ddcfe64971b5ddc9a90a4b9
songhee <thdgml5537@gmail.com>
2021-02-15 23:33:43 -0500

    add styling on landing page

--- client/src/component/UserHomePage/UserHomePage.jsx
+++ client/src/component/UserHomePage/UserHomePage.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import {NavLink} from 'react-router-dom'
+import "./UserHomePage.css"
+// import API from './'
 
 
 const UserHomePage = () => {
   return (
-    <div className="home-wrap">
-      <div className="home-container container">
-        <h2 className="home-title has-text-centered">Welcome User!</h2>
-        <h3 className="home-subtitle has-text-centered is-uppercase">
+    <div className="user__landing__page__container">
+      <video src="/videos/cat.mp4" autoPlay loop muted />
+      <div className="user__landing__page container">
+        <h2 className="user__landing__page__title">Welcome User!</h2>
+        <h3 className="user__landing__page__subtitle ">
           <strong>
-           Visit your Account to see more options<br />
-           <button class="button is-large is-fullwidth">Account</button>
-          
-            <br /> 
+           Visit your Account to see more options
+           <NavLink to='/AccountManage' className="button is-large">Account</NavLink>
           </strong>
         </h3>
       </div>
