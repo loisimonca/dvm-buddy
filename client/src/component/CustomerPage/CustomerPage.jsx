bd69d2bcdb4c09cb06630d7b7818771d370ac687
chyppoli <claude.hyppolite@gmail.com>
2021-02-17 19:48:05 -0500

    remove image from alt

--- client/src/component/CustomerPage/CustomerPage.jsx
+++ client/src/component/CustomerPage/CustomerPage.jsx
@@ -9,7 +9,29 @@ const CustomerPage = (props) => {
           <figure className="image is-4by3">
             <img
               src="https://bulma.io/images/placeholders/1280x960.png"
-              alt="Placeholder image"
+              alt="Placeholder"
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left"></div>
+            <div className="media-content">
+              <p className="title is-4"></p>
+              <p className="subtitle is-6"></p>
+            </div>
+          </div>
+          <div className="content">
+            <br />
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src="https://bulma.io/images/placeholders/1280x960.png"
+              alt="Placeholder"
             />
           </figure>
         </div>
@@ -53,7 +75,7 @@ const CustomerPage = (props) => {
           <figure className="image is-4by3">
             <img
               src="https://bulma.io/images/placeholders/1280x960.png"
-              alt="Placeholder image"
+              alt="Placeholder"
             />
           </figure>
         </div>
@@ -75,29 +97,7 @@ const CustomerPage = (props) => {
           <figure className="image is-4by3">
             <img
               src="https://bulma.io/images/placeholders/1280x960.png"
-              alt="Placeholder image"
-            />
-          </figure>
-        </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-left"></div>
-            <div className="media-content">
-              <p className="title is-4"></p>
-              <p className="subtitle is-6"></p>
-            </div>
-          </div>
-          <div className="content">
-            <br />
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img
-              src="https://bulma.io/images/placeholders/1280x960.png"
-              alt="Placeholder image"
+              alt="Placeholder"
             />
           </figure>
         </div>
